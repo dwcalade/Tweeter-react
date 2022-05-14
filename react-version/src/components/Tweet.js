276973eb7c80d3e8365e711b3c6fb8fe4965dad0
dwcalade <dwcalade@yahoo.ca>
2022-05-14 01:46:49 -0700

    Handling Events In React

---
+++ react-version/src/components/Tweet.js
@@ -0,0 +1,8 @@
+const tweet = {
+    avatar: "https://i.imgur.com/73hZDYK.png",
+    firstName: "Isaac",
+    lastName: "Newton",
+    handle: "@SirIsaac",
+    body: "If I have seen further it is by standing on the shoulders of giants",
+    age: "10 days ago"
+  }
